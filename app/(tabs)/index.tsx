c7416fbc99db99e77ff14a9c1856ac92ec3f5928
gyb <gyb984571377@gmail.com>
2024-11-26 11:36:48 +0800

    gesture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 import { useState } from 'react';
 import { type ImageSource } from 'expo-image';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import * as ImagePicker from 'expo-image-picker';
 import ImageViewer from "@/components/ImageViewer";
@@ -53,7 +54,7 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -75,7 +76,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
